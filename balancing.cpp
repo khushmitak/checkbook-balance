49e8f7c26add0570f90e9867ade8a5dc449f5c1e
Khushmita Kaur <khushp9894@gmail.com>
2024-04-03 22:27:26 -0700

    implement money class functions

--- balancing.cpp
+++ balancing.cpp
@@ -24,3 +24,51 @@ public:
 private:
   long all_cents;
 };
+
+// Money class begin
+
+Money::Money() : all_cents(0) {}
+
+Money::Money(long dollars) : all_cents(dollars * 100) {}
+
+Money::Money(long dollars, int cents) {
+  if (dollars * cents < 0) {
+    cout << "Illegal values for dollars and cents.\n";
+    exit(1);
+  }
+
+  all_cents = dollars * 100 + cents;
+}
+
+double Money::get_value() const { return all_cents * 0.01; }
+
+Money operator+(const Money &amount1, const Money &amount2) {
+  Money temp;
+  temp.all_cents = amount1.all_cents + amount2.all_cents;
+
+  return temp;
+}
+
+Money operator-(const Money &amount1, const Money &amount2) {
+  Money temp;
+  temp.all_cents = amount1.all_cents - amount2.all_cents;
+
+  return temp;
+}
+
+Money operator-(const Money &amount) {
+  Money temp;
+  temp.all_cents = -amount.all_cents;
+
+  return temp;
+}
+
+bool operator==(const Money &amount1, const Money &amount2) {
+  return amount1.all_cents == amount2.all_cents;
+}
+
+Money &Money::operator+=(const Money &rhs) {
+  this->all_cents += rhs.all_cents;
+
+  return *this;
+}
