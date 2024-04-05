dd83c8bc8f34d5c05f3b7aad9ebc2a27ca5e8755
Khushmita Kaur <khushp9894@gmail.com>
2024-04-04 23:26:19 -0700

    add streams

--- balancing.cpp
+++ balancing.cpp
@@ -72,3 +72,62 @@ Money &Money::operator+=(const Money &rhs) {
 
   return *this;
 }
+
+istream &operator>>(istream &ins, Money &amount) {
+  char one_char, decimal_point, digit1, digit2;
+  long dollars;
+  int cents;
+  bool negative;
+
+  ins >> one_char;
+
+  if (one_char == '-') {
+    negative = true;
+    ins >> one_char; // read '$'
+  } else {
+    negative = false;
+  }
+
+  ins >> dollars >> decimal_point >> digit1 >> digit2;
+
+  if (one_char != '$' || decimal_point != '.' || !isdigit(digit1) ||
+      !isdigit(digit2)) {
+    cout << "Error illegal form for money input\n";
+    exit(1);
+  }
+
+  cents = digits_to_int(digit1) * 10 + digits_to_int(digit2);
+  amount.all_cents = dollars * 100 + cents;
+
+  if (negative) {
+    amount.all_cents = -amount.all_cents;
+  }
+
+  return ins;
+}
+
+ostream &operator<<(ostream &outs, const Money &amount) {
+  long positive_cents, dollars, cents;
+  positive_cents = labs(amount.all_cents);
+  dollars = positive_cents / 100;
+  cents = positive_cents % 100;
+
+  if (amount.all_cents < 0) {
+    outs << "-$" << dollars << '.';
+  } else {
+    outs << '$' << dollars << '.';
+  }
+
+  if (cents < 10) {
+    outs << '0';
+  }
+
+  outs << cents;
+
+  return outs;
+}
+// Money Class ends
+
+int digits_to_int(char c) {
+  return static_cast<int>(c) - static_cast<int>('0');
+}
